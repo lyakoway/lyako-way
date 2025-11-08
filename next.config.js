6a4016d637d4dd4e558b04091776dba218fe091a
aamazurenk <aamazurenk@mts.ru>
2025-11-08 18:52:40 +0300

    SWC вместо Babel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Включаем строгий режим React
+
   images: {
-    domains: ["images.vexels.com"],
+    domains: ["images.vexels.com"], // Разрешённые домены для <Image />
   },
+
   webpack(config) {
+    // Обработка SVG через SVGR и url-loader
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.tsx?$/,
@@ -11,16 +16,17 @@ module.exports = {
 
     return config;
   },
-  // pageExtensions: ["page.tsx", "page.js"],
+
+  compiler: {
+    styledComponents: true, // SWC-трансформация styled-components
+  },
+
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false, // Ошибки ESLint блокируют сборку
   },
   typescript: {
-    // TODO: выключить этот флаг, когда будут исправлены все ошибки
-    ignoreBuildErrors: true,
-  },
-  compiler: {
-    // Enables the styled-components SWC transform
-    styledComponents: true,
+    ignoreBuildErrors: false,   // Ошибки TS блокируют сборку
   },
 };
+
+module.exports = nextConfig;
