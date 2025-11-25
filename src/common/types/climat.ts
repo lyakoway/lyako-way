a16919467c9024ad129e83351ed0eebedf864b18
aamazurenk <aamazurenk@mts.ru>
2025-11-25 12:40:52 +0300

    Рефактор

--- src/common/types/climat.ts
+++ src/common/types/climat.ts
@@ -6,32 +6,34 @@ export type ClimateType =
   | "cloudyWithRainAndLightning"
   | "snowy";
 
-
 export interface Weather {
-    location: {
-        name: string;
-        region: string;
-        country: string;
-        localtime: string;
-    };
-    current: {
-        temp_c: number;
-        feelslike_c: number;
-        condition: { text: string; icon: string };
-        humidity: number;
-        wind_kph: number;
-        wind_dir: string;
-        pressure_mb: number;
-        uv: number;
-    };
+  location: {
+    name: string;
+    region: string;
+    country: string;
+    localtime: string;
+    lat: number;
+    lon: number;
+  };
+  current: {
+    temp_c: number;
+    feelslike_c: number;
+    condition: { text: string; icon: string };
+    humidity: number;
+    wind_kph: number;
+    wind_dir: string;
+    pressure_mb: number;
+    uv: number;
+    is_day?: number;
+  };
 }
 
 export interface ForecastItem {
-    dt_txt: string;
-    main: { temp: number };
-    weather: { description: string; icon: string }[];
+  dt_txt: string;
+  main: { temp: number };
+  weather: { description: string; icon: string }[];
 }
 
 export interface ForecastResponse {
-    list: ForecastItem[];
+  list: ForecastItem[];
 }
