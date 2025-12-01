fb0dd15666c4dd7ab1bd86687b234fdb67bdecbf
aamazurenk <aamazurenk@mts.ru>
2025-12-01 16:54:00 +0300

    Рфактор темы

--- src/components/PagesSettings/index.tsx
+++ src/components/PagesSettings/index.tsx
@@ -1,35 +1,13 @@
-import React, { FC, useCallback, useEffect, useState } from "react";
+import React, { FC } from "react";
 
 import ButtonLang from "src/ui/ButtonLang";
 import ButtonHeart from "src/ui/ButtonHeart";
 import ThemeDarkLight from "src/ui/ThemeDarkLight";
-
-import { useDispatchTyped, useSelectorTyped } from "src/store";
-import { setThemeList } from "src/reducers";
-
 const PagesSettings: FC = () => {
-  const {
-    theme: { name },
-  } = useSelectorTyped(({ theme }) => theme);
-  const [openedTheme, setOpenedTheme] = useState(false);
-  const dispatch = useDispatchTyped();
-
-  const handleClickTheme = useCallback(() => {
-    setOpenedTheme(!openedTheme);
-    dispatch(setThemeList(!openedTheme));
-  }, [setOpenedTheme, dispatch, openedTheme]);
-
-  useEffect(() => {
-    setOpenedTheme(name === "light");
-  }, [name]);
-
   return (
     <>
       <ButtonHeart />
-      <ThemeDarkLight
-        handleClickTheme={handleClickTheme}
-        openedTheme={openedTheme}
-      />
+      <ThemeDarkLight />
       <ButtonLang />
     </>
   );
--- src/ui/ThemeDarkLight/index.tsx
+++ src/ui/ThemeDarkLight/index.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { ThemeDarkLightChecked } from "./style";
+import { useDispatchTyped, useSelectorTyped } from "src/store";
+import { setThemeList } from "src/reducers";
 
-const ThemeDarkLight = ({
-  handleClickTheme,
-  openedTheme,
-}: {
-  handleClickTheme: () => void;
-  openedTheme: boolean;
-}) => (
-  <ThemeDarkLightChecked
-    id="themeDarkLight"
-    $opened={openedTheme}
-    onClick={handleClickTheme}
-  />
-);
+const ThemeDarkLight = () => {
+  const {
+    theme: { name },
+  } = useSelectorTyped(({ theme }) => theme);
+  const [openedTheme, setOpenedTheme] = useState(name === "light");
+  const dispatch = useDispatchTyped();
+
+  const handleClickTheme = useCallback(() => {
+    setOpenedTheme(!openedTheme);
+    dispatch(setThemeList(!openedTheme));
+  }, [setOpenedTheme, dispatch, openedTheme]);
+
+  useEffect(() => {
+    setOpenedTheme(name === "light");
+  }, [name]);
+  return (
+    <ThemeDarkLightChecked
+      id="themeDarkLight"
+      $opened={openedTheme}
+      onClick={handleClickTheme}
+    />
+  );
+};
 
 export default ThemeDarkLight;
