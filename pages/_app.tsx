c69e588832ef6ed802ac7e217c36e50abd5109d6
aamazurenk <aamazurenk@mts.ru>
2026-07-14 23:57:14 +0300

    Убрать предупреждения в консоле

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,26 @@
 import React, { FC, useEffect } from "react";
 import Head from "next/head";
 
-import { useDispatchTyped, useSelectorTyped } from "src/store";
+import { Provider } from "react-redux";
 
 import { AppProps } from "next/app";
 
 import { ThemeProvider } from "styled-components";
 
+import { useDispatchTyped, useSelectorTyped, wrapper } from "src/store";
 import getAppHeadContent from "src/common/utils/getAppHeadContent";
 import GlobalStyles from "src/common/lib/globalStyles";
-import { wrapper } from "src/store";
 import { setThemeList } from "src/reducers";
 import { useDayTime } from "src/features/customHooks";
 import { Modal } from "src/ui/Modal";
 import { Toast } from "src/ui/Toast";
 
-const MyApp: FC = ({ Component, pageProps }: AppProps) => {
+// Внутренний компонент рендерится ВНУТРИ <Provider>, поэтому здесь
+// доступны redux-хуки (useSelectorTyped/useDispatchTyped).
+const AppContent: FC<{
+  Component: AppProps["Component"];
+  pageProps: AppProps["pageProps"];
+}> = ({ Component, pageProps }) => {
   const { theme } = useSelectorTyped(({ theme }) => theme);
   const dispatch = useDispatchTyped();
   const { dayTime } = useDayTime();
@@ -25,21 +30,32 @@ const MyApp: FC = ({ Component, pageProps }: AppProps) => {
   }, [dayTime]);
 
   return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Modal />
+      <Toast />
+    </ThemeProvider>
+  );
+};
+
+const MyApp = ({ Component, ...rest }: AppProps) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { pageProps } = props;
+
+  return (
     <React.StrictMode>
       <Head>
         <title>LYAKOWAY</title>
         {getAppHeadContent()}
       </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <main>
-          <Component {...pageProps} />
-        </main>
-        <Modal />
-        <Toast />
-      </ThemeProvider>
+      <Provider store={store}>
+        <AppContent Component={Component} pageProps={pageProps} />
+      </Provider>
     </React.StrictMode>
   );
 };
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
